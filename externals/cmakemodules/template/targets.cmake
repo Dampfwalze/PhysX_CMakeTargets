d6a4a716c39779bd8389d3e3f73b08dca4cac6bf
Dampfwalze <46897578+Dampfwalze@users.noreply.github.com>
2022-06-17 00:36:09 +0200

    Add: static/shared switch Update: lib pre- and suffixes are now adaptive

--- externals/cmakemodules/template/targets.cmake
+++ externals/cmakemodules/template/targets.cmake
@@ -4,33 +4,55 @@ if(NOT PHYSX_CURRENT_BINARY_DIR)
     set(PHYSX_CURRENT_BINARY_DIR "${__CMAKE_CURRENT_LIST_DIR}")
 endif()
 
-add_library(PhysX::PhysX SHARED IMPORTED)
-set_target_properties(PhysX::PhysX PROPERTIES
-    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/PhysX${EXE_SUFFIX}.dll"
-    IMPORTED_IMPLIB "${PHYSX_CURRENT_BINARY_DIR}/PhysX${EXE_SUFFIX}.lib"
-)
+if(${PX_GENERATE_STATIC_LIBRARIES})
+    add_library(PhysX::PhysX STATIC IMPORTED)
+    set_target_properties(PhysX::PhysX PROPERTIES
+        IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysX_static${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    )
+    target_link_libraries(PhysX::PhysX INTERFACE -ldl -pthread)
+else()
+    add_library(PhysX::PhysX SHARED IMPORTED)
+    set_target_properties(PhysX::PhysX PROPERTIES
+        IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_SHARED_LIBRARY_PREFIX}PhysX${EXE_SUFFIX}${CMAKE_SHARED_LIBRARY_SUFFIX}"
+        IMPORTED_IMPLIB "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysX${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    )
+endif()
 target_include_directories(PhysX::PhysX INTERFACE
     "${PHYSX_ROOT_DIR}/include"
     "${CMAKE_BINARY_DIR}/include"
     "${PXSHARED_PATH}/include"
 )
 
-add_library(PhysX::Common SHARED IMPORTED)
-set_target_properties(PhysX::Common PROPERTIES
-    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/PhysXCommon${EXE_SUFFIX}.dll"
-    IMPORTED_IMPLIB "${PHYSX_CURRENT_BINARY_DIR}/PhysXCommon${EXE_SUFFIX}.lib"
-)
+if(${PX_GENERATE_STATIC_LIBRARIES})
+    add_library(PhysX::Common STATIC IMPORTED)
+    set_target_properties(PhysX::Common PROPERTIES
+        IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXCommon_static${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    )
+else()
+    add_library(PhysX::Common SHARED IMPORTED)
+    set_target_properties(PhysX::Common PROPERTIES
+        IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_SHARED_LIBRARY_PREFIX}PhysXCommon${EXE_SUFFIX}${CMAKE_SHARED_LIBRARY_SUFFIX}"
+        IMPORTED_IMPLIB "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXCommon${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    )
+endif()
 target_include_directories(PhysX::Common INTERFACE
     "${PHYSX_ROOT_DIR}/include"
     "${CMAKE_BINARY_DIR}/include"
     "${PXSHARED_PATH}/include"
 )
 
-add_library(PhysX::Foundation SHARED IMPORTED)
-set_target_properties(PhysX::Foundation PROPERTIES
-    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/PhysXFoundation${EXE_SUFFIX}.dll"
-    IMPORTED_IMPLIB "${PHYSX_CURRENT_BINARY_DIR}/PhysXFoundation${EXE_SUFFIX}.lib"
-)
+if(${PX_GENERATE_STATIC_LIBRARIES})
+    add_library(PhysX::Foundation STATIC IMPORTED)
+    set_target_properties(PhysX::Foundation PROPERTIES
+        IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXFoundation_static${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    )
+else()
+    add_library(PhysX::Foundation SHARED IMPORTED)
+    set_target_properties(PhysX::Foundation PROPERTIES
+        IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_SHARED_LIBRARY_PREFIX}PhysXFoundation${EXE_SUFFIX}${CMAKE_SHARED_LIBRARY_SUFFIX}"
+        IMPORTED_IMPLIB "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXFoundation${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    )
+endif()
 target_include_directories(PhysX::Foundation INTERFACE
     "${PHYSX_ROOT_DIR}/include"
     "${CMAKE_BINARY_DIR}/include"
@@ -39,7 +61,7 @@ target_include_directories(PhysX::Foundation INTERFACE
 
 add_library(PhysX::Extensions STATIC IMPORTED)
 set_target_properties(PhysX::Extensions PROPERTIES
-    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/PhysXExtensions_static${EXE_SUFFIX}.lib"
+    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXExtensions_static${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
 )
 target_include_directories(PhysX::Extensions INTERFACE
     "${PHYSX_ROOT_DIR}/include"
@@ -49,7 +71,7 @@ target_include_directories(PhysX::Extensions INTERFACE
 
 add_library(PhysX::Task STATIC IMPORTED)
 set_target_properties(PhysX::Task PROPERTIES
-    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/PhysXTask_static${EXE_SUFFIX}.lib"
+    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXTask_static${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
 )
 target_include_directories(PhysX::Task INTERFACE
     "${PHYSX_ROOT_DIR}/include"
@@ -57,11 +79,11 @@ target_include_directories(PhysX::Task INTERFACE
     "${PXSHARED_PATH}/include"
 )
 
-add_library(PhysX::Vihicle STATIC IMPORTED)
-set_target_properties(PhysX::Vihicle PROPERTIES
-    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/PhysXVihicle_static${EXE_SUFFIX}.lib"
+add_library(PhysX::Vehicle STATIC IMPORTED)
+set_target_properties(PhysX::Vehicle PROPERTIES
+    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXVehicle_static${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
 )
-target_include_directories(PhysX::Vihicle INTERFACE
+target_include_directories(PhysX::Vehicle INTERFACE
     "${PHYSX_ROOT_DIR}/include"
     "${CMAKE_BINARY_DIR}/include"
     "${PXSHARED_PATH}/include"
@@ -69,8 +91,20 @@ target_include_directories(PhysX::Vihicle INTERFACE
 
 add_library(PhysX::Cooking STATIC IMPORTED)
 set_target_properties(PhysX::Cooking PROPERTIES
-    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/PhysXCooking_static${EXE_SUFFIX}.lib"
+    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXCooking_static${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
 )
+if(${PX_GENERATE_STATIC_LIBRARIES})
+    add_library(PhysX::Cooking STATIC IMPORTED)
+    set_target_properties(PhysX::Cooking PROPERTIES
+        IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXCooking_static${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    )
+else()
+    add_library(PhysX::Cooking SHARED IMPORTED)
+    set_target_properties(PhysX::Cooking PROPERTIES
+        IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_SHARED_LIBRARY_PREFIX}PhysXCooking${EXE_SUFFIX}${CMAKE_SHARED_LIBRARY_SUFFIX}"
+        IMPORTED_IMPLIB "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXCooking${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    )
+endif()
 target_include_directories(PhysX::Cooking INTERFACE
     "${PHYSX_ROOT_DIR}/include"
     "${CMAKE_BINARY_DIR}/include"
@@ -79,7 +113,7 @@ target_include_directories(PhysX::Cooking INTERFACE
 
 add_library(PhysX::CharacterKinematic STATIC IMPORTED)
 set_target_properties(PhysX::CharacterKinematic PROPERTIES
-    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/PhysXCharacterKinematic_static${EXE_SUFFIX}.lib"
+    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXCharacterKinematic_static${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
 )
 target_include_directories(PhysX::CharacterKinematic INTERFACE
     "${PHYSX_ROOT_DIR}/include"
@@ -89,7 +123,7 @@ target_include_directories(PhysX::CharacterKinematic INTERFACE
 
 add_library(PhysX::PvdSDK STATIC IMPORTED)
 set_target_properties(PhysX::PvdSDK PROPERTIES
-    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/PhysXPvdSDK_static${EXE_SUFFIX}.lib"
+    IMPORTED_LOCATION "${PHYSX_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}PhysXPvdSDK_static${EXE_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}"
 )
 target_include_directories(PhysX::PvdSDK INTERFACE
     "${PHYSX_ROOT_DIR}/include"
